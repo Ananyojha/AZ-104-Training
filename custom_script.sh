9cc3616e059983b8c27513db20c355523863e985
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-03-11 19:45:24 +0530

    Update custom_script.sh

--- custom_script.sh
+++ custom_script.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get install -y nginx
-echo $(hostname) | sudo tee /var/www/html/index.html
+echo "<html><head><title>Demo App on Ec2 / Azure VM</title></head><body><h1> Hey There 👋 you are served by $(hostname)</h1><br><h2>Pls visit /video in URL..</h2></body></html> " | sudo tee /var/www/html/index.html
 sudo mkdir -p /var/www/html/video
-echo "<html><head><title>My video Page</title></head><body><h1>Hello, world!</h1><h3>A video for you..</h3><iframe width="560" height="315" src="https://www.youtube.com/embed/3n5L3cGCbDo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></body></html>" | sudo tee /var/www/html/video/test.html
+echo "<html><head><title>My video Page</title></head><body><h1>Hello, world!</h1><h3>A video for you..</h3><iframe width="560" height="315" src="https://www.youtube.com/embed/teHR00DpX2E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></body></html>" > index.html
